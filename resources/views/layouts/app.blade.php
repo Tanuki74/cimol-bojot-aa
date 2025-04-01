fadb7d29236e3b3e4e13709923e409858d433197
Tanuki74 <bannyjuanda@gmail.com>
2025-04-01 19:00:53 +0700

    redesign background style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,14 +13,26 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <style type="text/css">
+            .pattern{
+                background: radial-gradient(
+                    closest-side,
+                    rgba(165, 158, 17, 1) 0%,
+                    rgba(152, 0, 0, 1) 100%
+                );
+                position: relative;
+                overflow: hidden;
+            }
+        </style>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen pattern">
             <livewire:layout.navigation />
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header>
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
